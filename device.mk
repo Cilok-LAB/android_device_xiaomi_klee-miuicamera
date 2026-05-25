5d9120a39a17ca7b433fcf8c152b1690d24cf14c
kylieeXD <khayloaftck@gmail.com>
2026-05-25 22:21:27 +0700

    configs: Add more permissions
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

--- device.mk
+++ device.mk
@@ -8,6 +8,10 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-miuicamera.xml
 
+# Default-app permisson
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/miuicamera-permissions.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/default-permissions/miuicamera-permissions.xml
+
 # Sysconfig
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/miuicamera-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/miuicamera-hiddenapi-package-allowlist.xml
